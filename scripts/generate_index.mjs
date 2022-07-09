2dbf34a4efb7dbf23fd9d356035b86dbb7ec351f
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-09 13:08:12 +0700

    Add script to generate index

---
+++ scripts/generate_index.mjs
@@ -0,0 +1,16 @@
+import glob from 'glob';
+import fs from 'fs';
+import matter from 'gray-matter';
+
+const pages = {};
+
+for (const file of glob.sync('**/*.md', { cwd: 'wiki' })) {
+  const slug = file.replace(/\.md$/, '');
+  const meta = matter(fs.readFileSync(`wiki/${file}`, 'utf8'));
+  const name = meta.data.name || undefined;
+  pages[slug] = { name };
+  console.log('Added', slug, name);
+}
+
+const index = { pages };
+fs.writeFileSync('index.json', JSON.stringify(index, null, 2));
