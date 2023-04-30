5c33c984d1a43eb891fd2756a9f7dc77370b31ad
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-30 21:19:54 +0700

    add migration script for previous change

--- scripts/formatFrontMatter.mjs
+++ scripts/formatFrontMatter.mjs
@@ -1,19 +1,38 @@
-import { globbySync } from "globby";
-import matter from "gray-matter";
-import prettier from "prettier";
-import { dump } from "js-yaml";
-import fs from "fs";
+import { globbySync } from 'globby'
+import matter from 'gray-matter'
+import prettier from 'prettier'
+import { dump } from 'js-yaml'
+import fs from 'fs'
 
-for (const file of globbySync("wiki/**/*.md")) {
-  const data = matter.read(file);
-  if (JSON.stringify(data.data) === "{}") continue;
+function transformFrontMatter(data, file) {
+  // You can programmatically process the front matter here.
+  // This is useful for migrating the structure of the front matter across the site.
+
+  // /* 2023-04-30: Remove the top-level keys of event pages after moving them into a nested object. */
+  // if (file.startsWith('wiki/Events/') && data.event) {
+  //   delete data.name
+  //   delete data.location
+  //   delete data.date
+  //   delete data.endDate
+  //   delete data.site
+  //   delete data.hosts
+  // }
+
+  return data
+}
+
+for (const file of globbySync('wiki/**/*.md')) {
+  const data = matter.read(file)
+  if (JSON.stringify(data.data) === '{}') continue
   const output = [
-    "---",
+    '---',
     prettier
-      .format(dump(data.data, { lineWidth: -1 }), { parser: "yaml" })
+      .format(dump(transformFrontMatter(data.data, file), { lineWidth: -1 }), {
+        parser: 'yaml',
+      })
       .trimEnd(),
-    "---",
-    data.content.replace(/\r\n/g, "\n"),
-  ].join("\n");
-  fs.writeFileSync(file, output);
+    '---',
+    data.content.replace(/\r\n/g, '\n'),
+  ].join('\n')
+  fs.writeFileSync(file, output)
 }
